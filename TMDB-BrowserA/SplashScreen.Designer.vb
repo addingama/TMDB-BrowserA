6fb1ded79c485d4a5f9a7d77110a34de2c7bb746
addingama <addingama@ADDINGAMABE6D73>
2022-10-05 09:28:10 +0800

    Add project files.

---
+++ TMDB-BrowserA/SplashScreen.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SplashScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.pb_loading = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lbl_percentage = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.TMDB_BrowserA.My.Resources.Resources._98432_loading
+        Me.PictureBox1.Location = New System.Drawing.Point(285, 49)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(231, 238)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("PMingLiU-ExtB", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(214, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(373, 32)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "The Movie Database Browser"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(358, 304)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Loading..."
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(437, 304)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 13)
+        Me.Label4.TabIndex = 4
+        '
+        'pb_loading
+        '
+        Me.pb_loading.Location = New System.Drawing.Point(84, 350)
+        Me.pb_loading.Name = "pb_loading"
+        Me.pb_loading.Size = New System.Drawing.Size(632, 23)
+        Me.pb_loading.Step = 1
+        Me.pb_loading.TabIndex = 5
+        '
+        'Timer1
+        '
+        '
+        'lbl_percentage
+        '
+        Me.lbl_percentage.AutoSize = True
+        Me.lbl_percentage.Location = New System.Drawing.Point(418, 304)
+        Me.lbl_percentage.MinimumSize = New System.Drawing.Size(50, 0)
+        Me.lbl_percentage.Name = "lbl_percentage"
+        Me.lbl_percentage.Size = New System.Drawing.Size(50, 13)
+        Me.lbl_percentage.TabIndex = 6
+        Me.lbl_percentage.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 412)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.lbl_percentage)
+        Me.Controls.Add(Me.pb_loading)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents pb_loading As ProgressBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents lbl_percentage As Label
+End Class
---
+++ TMDB-BrowserA/SplashScreen.vb
@@ -0,0 +1,33 @@
+﻿Public Class SplashScreen
+    'Variable global untuk progress yang akan di update setiap interval
+    Dim progress As Integer = 0
+
+    'Fungsi yang dieksekusi setiap interval
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'Dapatkan nilai progress yang baru
+        Dim newProgress = progress + 1
+
+        'Periksa apakah progress kurang dari atau sama dengan 100
+        If (newProgress <= 100) Then
+            ' update tulisan pada label
+            lbl_percentage.Text = newProgress & "%"
+            ' update progress bar value
+            pb_loading.Value = newProgress
+            ' update global variable progress supaya angkanya tidak 0 terus
+            progress = newProgress
+        Else
+            ' kalau newProgress sudah lebih dari 100 maka timer stop
+            Timer1.Stop()
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
